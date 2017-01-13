c6d94f1a8b657aa251736155555d4848b59528e4
Lutgardo Jiménez <lutgardo1983@hotmail.com>
2017-01-13 14:11:14 +0100

    Se añade el crud para dar de alta, borrar modificar las pizarras y los notas

--- app/src/main/java/com/luti/seccion_04_realm/activities/BoardActivity.java
+++ app/src/main/java/com/luti/seccion_04_realm/activities/BoardActivity.java
@@ -6,7 +6,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -55,6 +58,7 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
                showAlertForCreatingBoard("Add new board", "Type a name for your new board");
            }
        });
+        registerForContextMenu(listView);
 
         //PARA DEJAR VACIA LA BBDD
 //        realm.beginTransaction();
@@ -68,6 +72,26 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
         realm.beginTransaction();
         Board board = new Board(boardName);
         realm.copyToRealm(board);
+        realm.commitTransaction();
+
+    }
+    //Borrar de la base de datos un board seleccionado
+    private void deleteBoard(Board board){
+        realm.beginTransaction();
+        board.deleteFromRealm();
+        realm.commitTransaction();
+
+    }
+    private void editBoard(String newName, Board board){
+        realm.beginTransaction();
+        board.setTitle(newName);
+        realm.copyToRealmOrUpdate(board);
+        realm.commitTransaction();
+
+    }
+    private void deleteAll(){
+        realm.beginTransaction();
+        realm.deleteAll();
         realm.commitTransaction();
 
     }
@@ -99,6 +123,87 @@ public class BoardActivity extends AppCompatActivity implements RealmChangeListe
         });
         AlertDialog dialog = builder.create();
         dialog.show();
+
+    }
+
+    //Alert para crear la ventana de edicion de un board
+    private void showAlertForEditingBoard(String title, String message, final Board board){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this); //instancia del builder pop up con input
+        if(title != null) builder.setTitle(title);
+        if(message != null) builder.setMessage(message);
+
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_board,null);
+        builder.setView(viewInflated);
+
+        //Ponemos la vista inflada que va a ser la que va a tener el EditText del popup
+        final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewBoard);
+        input.setText(board.getTitle());
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String boardName = input.getText().toString().trim();
+                if (boardName.length() == 0){
+                    Toast.makeText(getApplicationContext(), "The name is required to edit the current Board", Toast.LENGTH_SHORT).show();
+                }
+                else if(boardName.equals(board.getTitle())){
+                    Toast.makeText(getApplicationContext(), "The name is the same that it was before", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                         editBoard(boardName, board);
+                }
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    /*Events */
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_board_activity, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.delete_all:
+                deleteAll();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        menu.setHeaderTitle(boards.get(info.position).getTitle());
+        getMenuInflater().inflate(R.menu.context_menu_board_activity,menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+
+        switch (item.getItemId()) {
+            case R.id.delete_board:
+                deleteBoard(boards.get(info.position));
+                return true;
+            case R.id.edit_board:
+                showAlertForEditingBoard("Edit Board", "Change the name of the board", boards.get(info.position));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+
 
     }
 
--- app/src/main/java/com/luti/seccion_04_realm/activities/NoteActivity.java
+++ app/src/main/java/com/luti/seccion_04_realm/activities/NoteActivity.java
@@ -5,8 +5,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -67,7 +71,8 @@ public class NoteActivity extends AppCompatActivity  implements RealmChangeListe
                 showAlertForCreatingNote("Add New Note","Type a note for " +board.getTitle() + "." );
             }
         });
-
+        //Muy importante poner esto para que me muestre el context menu al pulsar sobre el item
+        registerForContextMenu(listView);
 
     }
 
@@ -100,6 +105,37 @@ public class NoteActivity extends AppCompatActivity  implements RealmChangeListe
 
     }
 
+    //Creamos el dialogo para editar una nota
+
+    private void showAlertForEditNote(String title, String message, final Note notas){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this); //instancia del builder pop up con input
+        if(title != null) builder.setTitle(title);
+        if(message != null) builder.setMessage(message);
+
+        View viewInflated = LayoutInflater.from(this).inflate(R.layout.dialog_create_note,null);
+        builder.setView(viewInflated);
+
+        //Ponemos la vista inflada que va a ser la que va a tener el EditText del popup
+        final EditText input = (EditText) viewInflated.findViewById(R.id.editTextNewNote);
+        input.setText(notas.getDescription());
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String note = input.getText().toString().trim();
+                if(notas.getDescription().equals(note)){
+                    Toast.makeText(getApplicationContext(), "The note is equals", Toast.LENGTH_LONG).show();
+                }
+                else
+                    editNote(note, notas);
+
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
     private void createNewNote(String note){
         realm.beginTransaction();
 
@@ -108,6 +144,70 @@ public class NoteActivity extends AppCompatActivity  implements RealmChangeListe
         //Guardamos la relacion con el board al que pertenece la nota
         board.getNotes().add(_note);
         realm.commitTransaction();
+
+    }
+
+    private void editNote(String newNoteDescription, Note note){
+        realm.beginTransaction();
+        note.setDescription(newNoteDescription);
+        realm.copyToRealmOrUpdate(note);
+        realm.commitTransaction();
+    }
+    private void deleteNote(Note note){
+        realm.beginTransaction();
+        note.deleteFromRealm();
+        realm.commitTransaction();
+    }
+
+    private void deleteAll(){
+        realm.beginTransaction();
+        board.getNotes().deleteAllFromRealm();
+        realm.commitTransaction();
+
+    }
+
+
+    /*Events*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.delete_note_All:
+                deleteAll();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.context_menu_note_activity, menu);
+
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()){
+            case R.id.delete_note:
+                deleteNote(notes.get(info.position));
+                return true;
+            case R.id.edit_note:
+                showAlertForEditNote("Edit Note", "Change description of the note", notes.get(info.position));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
 
     }
 
